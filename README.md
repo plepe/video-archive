989e50042229f284566e38667ec96b26747c99db
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-06-14 19:48:08 +0200

    Submodules + basic configuration

---
+++ README.md
@@ -0,0 +1,7 @@
+# INSTALLATION
+```sh
+git clone https://github.com/plepe/video-archive
+cd video-archive
+cp conf.php-dist conf.php
+git submodule update --init
+```
---
+++ modulekit.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Video Archive";
+$id = "video-archive";
+$depend = array("modulekit-form"); // use modulekit-form and all its requirements
+$include = array(
+  'php' => array(
+    'inc/*.php' // automatically include all files in inc-directory
+  ),
+  'js' => array(
+    'inc/*.js' // automatically include all files in inc-directory
+  ),
+  'css' => array(
+    'style.css' // include style.css
+  )
+);

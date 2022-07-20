7492f0319270d0d0443c3b620fbdf1f507f0cad1
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2022-07-20 14:01:47 +0200

    Database access

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const database = require('./src/database')
+
 const config = JSON.parse(fs.readFileSync('conf.json'))
+database.init(config)
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'twig')
@@ -15,9 +18,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/view/:id', (req, res) => {
-  res.render('index', {
-    message: 'params: ' + JSON.stringify(req.params) + ' query: ' + JSON.stringify(req.query)
-  })
+  database.query('select * from entity where id=?', [ req.params.id ],
+    (err, result) => {
+      if (err) {
+        console.error(err)
+      }
+
+      res.render('index', { message: JSON.stringify(result) })
+    })
+
 })
 
 app.use('/static', express.static('static'))
---
+++ src/database.js
@@ -0,0 +1,27 @@
+const Database = require('database-js').Connection
+
+class DB {
+  init (config) {
+    if (config.db) {
+      this.db = new Database(config.db)
+    } else {
+      this.db = new Database('sqlite:///' + config.data_dir + '/db.sqlite')
+    }
+
+    this.prepared = {}
+  }
+
+  query (qry, params, callback) {
+    if (!(qry in this.prepared)) {
+      this.prepared[qry] = this.db.prepareStatement(qry)
+    }
+
+    const stmt = this.prepared[qry]
+
+    stmt.execute(...params)
+      .then(results => callback(null, results))
+      .catch(reason => callback(reason))
+  }
+}
+
+module.exports = new DB()

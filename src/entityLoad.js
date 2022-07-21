e4675d17670111938238c6131d38e572c9e8e924
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-07-21 10:30:00 +0200

    entityLoad: load additional fields

--- src/entityLoad.js
+++ src/entityLoad.js
@@ -1,10 +1,55 @@
+//        async.mapSeries(result,
+//          (data, done) => loadEntityProperties(data, done),
+//          (err, result) => callback(err, result)
+//        )
+const async = require('async')
+
 const database = require('./database')
+
+const entityDataDef = {
+  Video: {
+    table: 'video',
+    properties: [ 'title', 'filesize', 'duration', 'originalFile' ]
+  }
+}
 
 module.exports = function entityLoad (id, callback) {
   database.query('select * from entity where id=?', [ id ],
   (err, result) => {
-    if (err) { return console.error(err) }
+    if (err) { return callback(err) }
 
-    callback(null, result[0])
+    if (!result.length) {
+      return callback(null, null)
+    }
+
+    loadEntityProperties(result[0], callback)
   })
+}
+
+function loadEntityProperties (data, callback) {
+  if (!(data.class in entityDataDef)) {
+    return callback(null, data)
+  }
+
+  const def = entityDataDef[data.class]
+
+  if (def.table) {
+    database.query('select * from ' + def.table + ' where id=?', [ data.id ],
+      (err, result) => {
+        if (err) { return callback(err) }
+
+        if (!result.length) {
+          return callback(null, data)
+        }
+
+        def.properties.forEach(key => {
+          data[key] = result[0][key]
+        })
+
+        callback(null, data)
+      }
+    )
+  } else {
+    callback(null, data)
+  }
 }

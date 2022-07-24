f96e08ada77e3b1132adc290845afc2fbd2a59b8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-07-24 20:19:59 +0200

    entityLoad (Browser): save()

--- src/entityLoadBrowser.js
+++ src/entityLoadBrowser.js
@@ -23,5 +23,18 @@ module.exports = {
       .then(res => res.json())
       .then(data => callback(null, data))
       .catch(reason => global.setTimeout(() => callback(reason), 0))
+  },
+
+  save (id, _class, data, callback) {
+    fetch('/api/' + id, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data, null, 2)
+    })
+      .then(res => res.json())
+      .then(data => callback(null, data))
+      .catch(reason => global.setTimeout(() => callback(reason), 0))
   }
 }
